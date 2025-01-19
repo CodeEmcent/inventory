b8da7396083cc7d5278a391e1e0980b93b52ecb6
Emcent <mcinnobezzy@gmail.com>
2025-01-19 11:18:56 +0100

    Improved Some Endpoints for Interactions with Frontend

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
-from accounts.serializers import CustomTokenObtainPairView
+from accounts.views import CustomTokenObtainPairView
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path("admin/", admin.site.urls),
     path("api/users/", include("accounts.urls")),
